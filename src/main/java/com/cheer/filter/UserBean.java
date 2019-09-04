1eef8a903e98da93bb23c194085b471ebc308471
yinchu <javayinchu2013@163.com>
2019-09-04 23:27:15 +0800

    updated

---
+++ src/main/java/com/cheer/filter/UserBean.java
@@ -0,0 +1,53 @@
+package com.cheer.filter;
+
+import java.io.Serializable;
+
+public class UserBean implements Serializable {
+
+	/**
+	 * 在进行反序列化时，JVM会把传来的字节流中的serialVersionUID于本地相应实体类的serialVersionUID进行比较。
+	 * 如果相同说明是一致的，可以进行反序列化，否则会出现反序列化版本一致的异常，即是InvalidCastException。
+	 */
+	private static final long serialVersionUID = -7998355985790156892L;
+
+	private String name;
+	private String password;
+	private int age;
+	private char gender;
+
+	public UserBean() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+}
